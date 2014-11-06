9b33d870e34e652015147170fcb6044143288c5f
Zack Guo <gizak@icloud.com>
2014-11-05 19:32:58 -0500

    Just get it working

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject om-mini-form "0.1.0-SNAPSHOT"
+  :description "Om in-page form component"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [om "0.8.0-alpha1"]
+                 [prismatic/om-tools "0.3.6"]])
